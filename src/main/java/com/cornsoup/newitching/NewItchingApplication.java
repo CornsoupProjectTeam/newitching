7656c5677f6271a6294d99a1a3a20eb6d837a599
ujin6666 <ujin736@gmail.com>
2025-04-03 22:04:52 +0900

    Remove @EnableAsync annotation

--- src/main/java/com/cornsoup/newitching/NewItchingApplication.java
+++ src/main/java/com/cornsoup/newitching/NewItchingApplication.java
@@ -2,10 +2,8 @@ package com.cornsoup.newitching;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
-@EnableAsync
 public class NewItchingApplication {
 
 	public static void main(String[] args) {
