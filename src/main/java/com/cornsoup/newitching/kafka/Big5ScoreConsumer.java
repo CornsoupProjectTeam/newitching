7323d2129aa7bd14156503f6b8c198e4b03ea6c0
ujin6666 <ujin736@gmail.com>
2025-04-03 22:01:03 +0900

    Merge With Another Service

--- src/main/java/com/cornsoup/newitching/kafka/Big5ScoreConsumer.java
+++ src/main/java/com/cornsoup/newitching/kafka/Big5ScoreConsumer.java
@@ -1,7 +1,7 @@
 package com.cornsoup.newitching.kafka;
 
 import com.cornsoup.newitching.kafka.dto.Big5ScoreMessage;
-import com.cornsoup.newitching.service.Big5ScoreService;
+import com.cornsoup.newitching.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -12,11 +12,11 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class Big5ScoreConsumer {
 
-    private final Big5ScoreService big5ScoreService;
+    private final MemberService memberService;
 
     @KafkaListener(topics = "big5_scores", groupId = "springboot-group")
     public void consume(Big5ScoreMessage message) {
         log.info("BIG5 점수 수신: {}", message.getMemberId());
-        big5ScoreService.updateBig5Scores(message);
+        memberService.updateBig5Scores(message);
     }
 }
