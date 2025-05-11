16023dff984b3a77f07515dc6899c93353fe13c2
ujin6666 <ujin736@gmail.com>
2025-05-11 15:29:55 +0900

    Add MemberDto

---
+++ src/main/java/com/cornsoup/newitching/dto/MemberDto.java
@@ -0,0 +1,11 @@
+package com.cornsoup.newitching.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class MemberDto {
+    private String department;
+    private String name;
+}
