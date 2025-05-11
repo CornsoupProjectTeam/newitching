1f6862319d133b568c574cf5a28d8e40190462ea
ujin6666 <ujin736@gmail.com>
2025-05-11 15:32:36 +0900

    Add consumer to subscribe to team matching results

---
+++ src/main/java/com/cornsoup/newitching/kafka/TeamMatchingResultConsumer.java
@@ -0,0 +1,30 @@
+package com.cornsoup.newitching.kafka;
+
+import com.cornsoup.newitching.kafka.dto.TeamResultMessage;
+import com.cornsoup.newitching.service.TeamService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class TeamMatchingResultConsumer {
+
+    private final TeamService teamService;
+
+    @KafkaListener(
+            topics = "team_matching_results",
+            groupId = "springboot-group",
+            properties = {
+                    "spring.json.value.default.type=com.cornsoup.newitching.kafka.dto.TeamResultMessage",
+                    "spring.json.trusted.packages=com.cornsoup.newitching.kafka.dto"
+            }
+    )
+    @KafkaListener(topics = "team_matching_results", groupId = "springboot-group")
+    public void consume(TeamResultMessage message) {
+        log.info("팀 매칭 결과 수신 - matchingId: {}", message.getMatchingId());
+        teamService.saveMatchingResults(message);
+    }
+}
